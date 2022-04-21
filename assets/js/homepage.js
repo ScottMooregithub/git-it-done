1cead23615becf0f6a08d3b8043dfe4db964b202
Scott Moore <sm91srjc@gmail.com>
2022-04-21 01:27:04 -0700

    completed git-it-on

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function (user) {
   //format the github api url
@@ -82,4 +83,30 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+isfeature&sort=help-wanted-issues";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub user Not found");
+    }
+  });
+};
+
+var buttonClickhandler = function (events) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = "";
+  }
+};
+
 userFormEl.addEventListener("submit", submitFormHandler);
+languageButtonsEl.addEventListener("click", buttonClickhandler);
